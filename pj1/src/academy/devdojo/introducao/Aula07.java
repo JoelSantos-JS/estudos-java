c7451386ef2cb0004f85b06deccc3fd672d7bd55
Joel Santos <joeltere9@gmail.com>
2023-03-07 11:33:56 -0300

    aula 07

--- pj1/src/academy/devdojo/introducao/Aula07.java
+++ pj1/src/academy/devdojo/introducao/Aula07.java
@@ -2,6 +2,30 @@ package academy.devdojo.introducao;
 
 public class Aula07 {
     public static void main(String[] args) {
+        byte name = 5;
+
+        switch(name){
+            case 1: System.out.println("Domingo");
+            break;
+            case 2: System.out.println("Segunda");
+            break;
+            case 3: System.out.println("Terca");
+            break;
+            case 4: System.out.println("Quarta");
+            break;
+            case 5: System.out.println("Quinta");
+            break;
+            case 6: System.out.println("Sexta");
+            break;
+        }
+
+        char sexo = 'M';
+
+        switch(sexo ){
+            case 'M': System.out.println("É homen");
+            break;
+            case 'F' : System.out.println("É mulher");
+        }
         
     }
 }
---
+++ pj1/src/academy/devdojo/introducao/Aula07Exercios.java
@@ -0,0 +1,18 @@
+package academy.devdojo.introducao;
+
+public class Aula07Exercios {
+    public static void main(String[] args) {
+        byte dias = 5;
+
+    switch (dias ) {
+        case 1:
+        case 7: System.out.println("Fim de semana");
+        break;
+        case 2 :
+        case 3 :
+        case 4 :
+        case 5 :
+        case 6 : System.out.println("Dia util");
+    } 
+    }
+}
