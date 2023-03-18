0c9ed959f482dc7923177ba7f2620e597248e36e
Joel Santos <joeltere9@gmail.com>
2023-03-18 18:29:37 -0300

    more update

--- pj1/src/academy/devdojo/javacore/QString/test/StringBuilderTest01.java
+++ pj1/src/academy/devdojo/javacore/QString/test/StringBuilderTest01.java
@@ -7,8 +7,11 @@ public class StringBuilderTest01 {
 
         System.out.println(nome);
 
-        StringBuilder sb = new StringBuilder("Joel jose ");
+        StringBuilder sb = new StringBuilder("Joel jose");
         sb.append("Dev");
+        sb.reverse();
+        sb.reverse();
+        sb.delete(0, 10);
         System.out.println(sb);
     }
 }
