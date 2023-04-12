efe94775ff6a10642442c8b69186b580bd6bb452
Joel Santos <joeltere9@gmail.com>
2023-04-12 14:56:31 -0300

    hash map

--- pj1/src/academy/devdojo/Nelio/Colections/test/MapTes.java
+++ pj1/src/academy/devdojo/Nelio/Colections/test/MapTes.java
@@ -1,18 +1,22 @@
 package academy.devdojo.Nelio.Colections.test;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class MapTes {
     public static void main(String[] args) {
         
-        Map<String , String> cookies = new TreeMap<>();
+        Map<String , String> cookies = new HashMap<>();
         cookies.put("0", "Chocolate" );
         cookies.put("1", "Coco");
 
-        cookies.remove("0");
         System.out.println(cookies);
+        cookies.remove("1");
+        cookies.put("5","abacate");
+        cookies.put("3","banana");
 
+        System.out.println(cookies);
         for ( String key : cookies.keySet()) {
             System.out.println(key);
         }
