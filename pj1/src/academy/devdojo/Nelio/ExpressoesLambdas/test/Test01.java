3498f2c5e9f0d0bd517cb450392064ee6fa2f39b
Joel Santos <joeltere9@gmail.com>
2023-04-13 16:47:55 -0300

    ex2 lambdas

--- pj1/src/academy/devdojo/Nelio/ExpressoesLambdas/test/Test01.java
+++ pj1/src/academy/devdojo/Nelio/ExpressoesLambdas/test/Test01.java
@@ -3,11 +3,13 @@ package academy.devdojo.Nelio.ExpressoesLambdas.test;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import academy.devdojo.Nelio.ExpressoesLambdas.domain.Product;
+import academy.devdojo.javacore.Jmodificadorfinal.dominio.Comparador;
 
 public class Test01 {
     public static void main(String[] args) {
@@ -29,9 +31,17 @@ public class Test01 {
                 line = bf.readLine();
             }
 
-         var list =    products.stream().map( x -> x.getPreco()).reduce(0.0 , (x, y) -> x + y / products.size());
+         double list =    products.stream().map( x -> x.getPreco()).reduce(0.0 , (x, y) -> x + y / products.size());
 
             System.out.println(list);
+
+            Comparator<String> comp = (s1, s2) ->  s1.toUpperCase().compareTo(s2.toUpperCase());
+
+            List<String> list2 = products.stream().filter( p -> p.getPreco() < list).map( p -> p.getNome()).sorted(comp.reversed()).collect(Collectors.toList());
+
+            for (String string : list2) {
+                System.out.println(string);
+            }
             
         } catch (Exception e) {
             // TODO: handle exception
