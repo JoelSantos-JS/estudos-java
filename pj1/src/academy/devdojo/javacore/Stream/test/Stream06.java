fd3dc0dd91a99cd17f35205a914d413fa59a497c
Joel Santos <joeltere9@gmail.com>
2023-04-19 17:39:04 -0300

    soma com stream e reduce

---
+++ pj1/src/academy/devdojo/javacore/Stream/test/Stream06.java
@@ -0,0 +1,20 @@
+package academy.devdojo.javacore.Stream.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import academy.devdojo.javacore.Stream.domain.Pessoa;
+
+public class Stream06 {
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = new ArrayList<>();
+        pessoas.add(new Pessoa(1 , "Joel", "Brasileiro" , 5));
+        pessoas.add(new Pessoa(2 , "Jose", "Brasileiro" , 25));
+        pessoas.add(new Pessoa(3 , "Marcos", "Brasileiro" , 65));
+        pessoas.add(new Pessoa(4 , "Uruguaio", "Uruguais" , 32));
+
+
+
+    pessoas.stream().filter(e -> e.getIdade() > 30).map(Pessoa::getIdade).reduce(Integer::sum).ifPresent(System.out::println);
+    }
+}
