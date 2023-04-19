32aceee555085ff5211547ac26360023721f5fba
Joel Santos <joeltere9@gmail.com>
2023-04-19 10:14:39 -0300

    day 19

---
+++ pj1/src/academy/devdojo/javacore/Stream/test/StreamTest03.java
@@ -0,0 +1,26 @@
+package academy.devdojo.javacore.Stream.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import academy.devdojo.javacore.Stream.domain.Pessoa;
+
+public class StreamTest03 {
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = new ArrayList<>();
+        pessoas.add(new Pessoa(1 , "Joel", "Brasileiro" , 5));
+        pessoas.add(new Pessoa(2 , "Jose", "Brasileiro" , 25));
+        pessoas.add(new Pessoa(3 , "Marcos", "Brasileiro" , 65));
+        pessoas.add(new Pessoa(4 , "Uruguaio", "Uruguais" , 32));
+
+
+       // pessoas.stream().forEach(e -> System.out.println(e.getNome()));
+        pessoas.stream().filter(e -> e.getNacionalidade().equals("Brasileiro")).forEach(e ->  System.out.println(e.getNome()));
+        pessoas.stream().filter(e -> e.getNome().equals("Joel")).map(Pessoa::getIdade).forEach(e -> System.out.println(e));
+
+      // System.out.println(pessoas);
+    }
+}
