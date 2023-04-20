375cf860749a4a4ebcb4d606d3949c8c8930db11
Joel Santos <joeltere9@gmail.com>
2023-04-20 17:43:00 -0300

    threads

--- pj1/src/academy/devdojo/javacore/Stream/test/StreamTest07.java
+++ pj1/src/academy/devdojo/javacore/Stream/test/StreamTest07.java
@@ -1,5 +1,30 @@
 package academy.devdojo.javacore.Stream.test;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import academy.devdojo.javacore.Stream.domain.Pessoa;
+
 public class StreamTest07 {
-    
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = new ArrayList<>();
+        pessoas.add(new Pessoa(1 , "Joel", "Brasileiro" , 5));
+        pessoas.add(new Pessoa(2 , "Jose", "Brasileiro" , 25));
+        pessoas.add(new Pessoa(3 , "Marcos", "Brasileiro" , 65));
+
+
+        System.out.println(pessoas.stream().count());
+
+        pessoas.stream().max(Comparator.comparing(Pessoa::getIdade)).ifPresent(System.out::println);
+
+      DoubleSummaryStatistics statistics =   pessoas.stream().collect(Collectors.summarizingDouble(Pessoa::getIdade));
+        
+    String steam=  pessoas.stream().map(e -> e.getNome()).collect(Collectors.joining(", "));
+      System.out.println(statistics);
+
+      System.out.println(steam);
+    }
 }
---
+++ pj1/src/academy/devdojo/javacore/Threads/test/ThreadTest01.java
@@ -0,0 +1,7 @@
+package academy.devdojo.javacore.Threads.test;
+
+public class ThreadTest01 {
+    public static void main(String[] args) {
+        
+    }
+}
