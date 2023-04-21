6abcc7b0ae1d977fd855ca3c904ef64e5db81107
Joel Santos <joeltere9@gmail.com>
2023-04-21 11:55:26 -0300

    inicio javafx

---
+++ pj1/src/academy/devdojo/Nelio/JavaFx/Program.java
@@ -0,0 +1,16 @@
+package academy.devdojo.Nelio.JavaFx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Program extends Application {
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+}
