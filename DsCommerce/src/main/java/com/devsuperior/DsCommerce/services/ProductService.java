a2023e38ae2dc9f0891f782b27dcccdaf04381ac
Joel Santos <joeltere9@gmail.com>
2023-05-03 14:21:58 -0300

    first commit day

--- DsCommerce/src/main/java/com/devsuperior/DsCommerce/services/ProductService.java
+++ DsCommerce/src/main/java/com/devsuperior/DsCommerce/services/ProductService.java
@@ -3,7 +3,8 @@ package com.devsuperior.DsCommerce.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,8 @@ import com.devsuperior.DsCommerce.dto.ProductDto;
 import com.devsuperior.DsCommerce.entities.Product;
 import com.devsuperior.DsCommerce.repository.ProductRepository;
 import com.devsuperior.DsCommerce.services.exceptions.ResourceNotFound;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service //registra a classe como um componente do spring, que vai ser injetado em outras classes
 public class ProductService {
@@ -60,23 +63,44 @@ public class ProductService {
     @Transactional
     public ProductDto put(Long id ,ProductDto dto) {
         
-        Product product2 = productRepository.getReferenceById(id);
-        product2.setName(dto.getName());
-        product2.setDescription(dto.getDescription());
-        product2.setImgUrl(dto.getImgUrl());
-        product2.setPrice(dto.getPrice());
 
-       product2 = productRepository.save(product2);
+        try {
+            Product product2 = productRepository.getReferenceById(id);
+            product2.setName(dto.getName());
+            product2.setDescription(dto.getDescription());
+            product2.setImgUrl(dto.getImgUrl());
+            product2.setPrice(dto.getPrice());
+    
+           product2 = productRepository.save(product2);
+
+           return  new ProductDto(product2);
+        } catch (EntityNotFoundException e) {
+           e.printStackTrace();
+           throw new ResourceNotFound("Recurso nao encontrado");
+        }
+       
 
 
-       return  new ProductDto(product2);
+   
 
     }
 
     @Transactional
     public void  deleteById(Long id) {
+
+        try {
+            productRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+            
+            throw new ResourceNotFound("Recurso nao encontrado");
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            throw new ResourceNotFound("Falha de intrigade referencial");
+        }
       
-        productRepository.deleteById(id);
+      
 
 
         
